6f2b540248416f6c47b71f8e75a42ad14f2e680e
Kevin <1147623296@qq.com>
2020-10-08 16:28:49 +0800

    编写main函数完成对栈的测试

--- Stack/src/per/zzb/stack/Main.java
+++ Stack/src/per/zzb/stack/Main.java
@@ -5,5 +5,21 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("This is my stack");
+
+        MyStack<Integer> stack = new MyStack<>(7);
+
+        for (int i = 0 ; i < 7 ; i ++){
+            stack.push(i);
+        }
+
+        System.out.println(stack);
+
+        stack.push(7);
+        System.out.println(stack);
+
+        stack.pop();stack.pop();stack.pop();stack.pop();stack.pop();
+        System.out.println(stack);
+
+        System.out.println(stack.peek());
     }
 }
