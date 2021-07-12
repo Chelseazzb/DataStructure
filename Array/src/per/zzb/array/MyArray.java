42cba283e8dbccb0812c005234c19815cd1315f0
Kevin <1147623296@qq.com>
2021-07-12 11:15:23 +0800

    对数组和栈的代码做一些维护和更新

--- Array/src/per/zzb/array/MyArray.java
+++ Array/src/per/zzb/array/MyArray.java
@@ -21,28 +21,28 @@ public class MyArray<E> {
      * @Param [capacity]
      * @Return
      **/
-    public MyArray(int capacity){
-        data = (E[])new Object[capacity];
+    public MyArray(int capacity) {
+        data = (E[]) new Object[capacity];
         size = 0;
     }
 
     //无参构造函数，默认开辟10的空间
-    public MyArray(){
+    public MyArray() {
         this(10);
     }
 
-    //获得数组的长度
-    public int getSize(){
+    //获得数组的当前长度
+    public int getSize() {
         return size;
     }
 
     //判断数组长度是否为空
-    public boolean isEmpty(){
-        return size == 0 ;
+    public boolean isEmpty() {
+        return size == 0;
     }
 
     //获取数组的容量
-    public int getCapacity(){
+    public int getCapacity() {
         return data.length;
     }
 
@@ -54,7 +54,7 @@ public class MyArray<E> {
      * @Param [e]
      * @Return void
      **/
-    public void addLast(E e){
+    public void addLast(E e) {
 //        if (size == data.length){
 //            throw new IllegalArgumentException("addLast failed, array is full");
 //        }
@@ -62,7 +62,7 @@ public class MyArray<E> {
 //        data[size] = e;
 //        size ++;
 
-        add(size,e); //函数复用
+        add(size, e); //函数复用
     }
 
     /**
@@ -73,29 +73,29 @@ public class MyArray<E> {
      * @Param [index, e]
      * @Return void
      **/
-    public void add(int index, E e){
-        if (index < 0 || index >= data.length){ //判断索引值大小
+    public void add(int index, E e) {
+        if (index < 0 || index >= data.length) { //判断索引值大小
             throw new IllegalArgumentException("add failed, argument is not allowed");
         }
 
-        if (size == data.length){ //判断数组是否已满
+        if (size == data.length) { //判断数组是否已满
 //            throw new IllegalArgumentException("add failed, array is full");
             resize(2 * data.length);
         }
 
-        for (int i = size - 1 ; i >= index ; i --){
+        for (int i = size - 1; i >= index; i--) {
             data[i + 1] = data[i]; //index之后的每个元素向后移动一位
         }
 
         data[index] = e; //用新的值覆盖掉原来的值
-        size ++; //数组的长度加1
+        size++; //数组的长度加1
     }
 
     //动态数组扩容
     private void resize(int newCapacity) {
-        E[] newData = (E[])new Object[newCapacity];
+        E[] newData = (E[]) new Object[newCapacity];
 
-        for (int i = 0 ; i < size ; i ++){
+        for (int i = 0; i < size; i++) {
             newData[i] = data[i];
         }
         data = newData;
@@ -103,12 +103,12 @@ public class MyArray<E> {
 
 
     //向数组中的第一个位置添加元素
-    public void addFirst(E e){
-        add(0,e); //函数复用
+    public void addFirst(E e) {
+        add(0, e); //函数复用
     }
 
     //获得数组中指定位置的元素
-    public E get(int index){
+    public E get(int index) {
         if (index < 0 || index >= data.length)
             throw new IllegalArgumentException("get failed, argument is not allowed");
 
@@ -116,7 +116,7 @@ public class MyArray<E> {
     }
 
     //修改数组中指定位置的元素
-    public void set(int index, E e){
+    public void set(int index, E e) {
         if (index < 0 || index >= data.length)
             throw new IllegalArgumentException("set failed, argument is not allowed");
 
@@ -124,21 +124,21 @@ public class MyArray<E> {
     }
 
     //查看数组中是否存在该元素以及存在的数量
-    public int contain(E e){
+    public int contain(E e) {
         int count = 0;
 
-        for(int i = 0 ; i < size ; i ++){
+        for (int i = 0; i < size; i++) {
             if (e == data[i])
-                count ++;
+                count++;
         }
 
         return count; //0代表没有
     }
 
-    //查找元素的位置
-    public int find(E e){
-        for (int i = 0 ; i < size ; i ++){
-            if (data[i].equals(e)){
+    //查找元素的第一个位置
+    public int find(E e) {
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(e)) {
                 return i;
             }
         }
@@ -146,29 +146,30 @@ public class MyArray<E> {
     }
 
     //查找数组中所有值为e的位置
-    public String findAll(E e){
-       String position = ""; //所有位置的字符串
+    public String findAll(E e) {
+        String position = ""; //所有位置的字符串
 
-        for (int i = 0 ; i < size ; i ++){
-            if (data[i].equals(e)){
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(e)) {
                 position += i;
-                position +=",";
+                position += ",";
             }
         }
-        return position;
+        //去掉字符串最后的,
+        return position.substring(0, position.length() - 1);
     }
 
     //删除数组中的元素
-    public E remove(int index){
+    public E remove(int index) {
         if (index < 0 || index >= data.length)
             throw new IllegalArgumentException("get failed, argument is not allowed");
 
         E deleteElement = data[index];
-        for (int i = index ; i < size ; i ++){
+        for (int i = index; i < size; i++) {
             data[i] = data[i + 1];
         }
 
-        size --;
+        size--;
         if (size == getCapacity() / 4 && getCapacity() / 2 != 0) { //注意这里的小bug
             resize(getCapacity() / 2);
         }
@@ -177,30 +178,30 @@ public class MyArray<E> {
     }
 
     //删除数组中第一个元素
-    public E removeFirst(){
+    public E removeFirst() {
         return remove(0);
     }
 
     //删除数组中最后一个元素
-    public E removeLast(){
+    public E removeLast() {
         return remove(size - 1);
     }
 
     //删除数组中指定元素的值第一个位置
-    public E removeElement(E e){
+    public E removeElement(E e) {
         int index = find(e);
         return remove(index);
     }
 
     //删除数组中所有值为e的元素
-    public void removeAllElement(E e){
+    public void removeAllElement(E e) {
         String position;
         position = findAll(e);
 
         String[] split = position.split(",");
 
         //考虑到每次删完一个元素都会向前移动，所以之前的位置也要变化!!!
-        for (int i = 0 ; i < split.length ; i ++) {
+        for (int i = 0; i < split.length; i++) {
             remove(Integer.parseInt(split[i]) - i);
         }
     }
@@ -209,12 +210,12 @@ public class MyArray<E> {
     @Override
     public String toString() {
         StringBuilder res = new StringBuilder();
-        res.append(String.format("data size is %d, capacity is %d\n",size,data.length));
+        res.append(String.format("data size is %d, capacity is %d\n", size, data.length));
         res.append("[");
-        for (int i = 0 ; i < size ; i ++){
+        for (int i = 0; i < size; i++) {
 
             res.append(data[i]); //向字符串里添加的不是index而是element！！！
-            if (i != size - 1){
+            if (i != size - 1) {
                 res.append(", ");
             }
         }
---
+++ Array/src/per/zzb/array/MyArrayTest.java
@@ -0,0 +1,75 @@
+package per.zzb.array;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * 动态数组的测试类
+ */
+class MyArrayTest {
+
+    @Test
+    void getSize() {
+    }
+
+    @Test
+    void isEmpty() {
+    }
+
+    @Test
+    void getCapacity() {
+    }
+
+    @Test
+    void addLast() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void addFirst() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void set() {
+    }
+
+    @Test
+    void contain() {
+    }
+
+    @Test
+    void find() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void removeFirst() {
+    }
+
+    @Test
+    void removeLast() {
+    }
+
+    @Test
+    void removeElement() {
+    }
+
+    @Test
+    void removeAllElement() {
+    }
+}
