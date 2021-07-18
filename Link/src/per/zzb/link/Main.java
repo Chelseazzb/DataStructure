80e28ab2ef6259d853f34a7a56e531c94819285a
Kevin <1147623296@qq.com>
2021-07-18 21:09:49 +0800

    完成了链表数据结构的全部功能

--- Link/src/per/zzb/link/Main.java
+++ Link/src/per/zzb/link/Main.java
@@ -9,4 +9,27 @@ package per.zzb.link;
  * @Version : 1.0
  **/
 public class Main {
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+
+        System.out.println("链表是否为空：" + linkedList.isEmpty());
+        System.out.println("链表的长度：" + linkedList.getSize());
+        System.out.println("获得链表中的第一个元素：" + linkedList.getFirst());
+        System.out.println("像链表中添加两个元素++++++++++++++");
+
+        linkedList.add(0, 1);
+        linkedList.addLast(2);
+
+//        linkedList.get(12);  //测试异常
+
+        System.out.println("获得链表中的第二个元素：" + linkedList.getLast());
+        linkedList.traverse();
+
+        linkedList.update(1, 3);
+        System.out.println(linkedList);
+
+        linkedList.deleteLast();
+        linkedList.deleteFirst();
+        linkedList.traverse();
+    }
 }
