7be45d7129c652449585bc43733e9e012b3af3ca
Kevin <1147623296@qq.com>
2021-07-18 15:44:13 +0800

    完成栈的leetcode试题

--- Stack/src/per/zzb/stack/Stack.java
+++ Stack/src/per/zzb/stack/Stack.java
@@ -3,13 +3,13 @@ package per.zzb.stack;
 //做成Stack的接口，一共包括五个接口
 public interface Stack<E> {
 
-    int getSize();
+    int getSize();  //获取数组容量
 
-    E pop();
+    E pop(); //弹出栈顶元素
 
-    void push(E e);
+    void push(E e); //向栈中加入元素
 
-    boolean isEmpty();
+    boolean isEmpty(); //检查栈是否为空
 
     E peek(); //查看栈顶元素
 }
--- Stack/src/per/zzb/stack/leetcode/Valid_Parentheses_20.java
+++ Stack/src/per/zzb/stack/leetcode/Valid_Parentheses_20.java
@@ -1,12 +1,61 @@
 package per.zzb.stack.leetcode;
 
+import java.util.Stack;
+
 /**
  * @ClassName : Valid_Parentheses_20
  * @Author : Zhai Zhibin
  * @Date : 2020/10/8 16:33
- * @Description : ToDo
+ * @Description : leetcode的第二十题，正确的括号
  * @Modified_by :
  * @Version : 1.0
  **/
 public class Valid_Parentheses_20 {
+
+    public boolean isValid(String s) {
+        Stack<Character> myStack = new Stack<>();
+
+        for (int i = 0 ; i < s.length() ; i ++){
+            Character c = s.charAt(i);
+
+            if (c == '(' || c == '[' || c =='{')
+                myStack.push(c);
+            else{ //需要考虑到一开始不是左边符号的情况
+                if (myStack.isEmpty() == true)
+                    return false;
+            }
+
+
+            if (c == ')'){
+                if (myStack.peek() != '(')
+                    return false;
+                else
+                    myStack.pop();
+            }
+
+            if (c == ']'){
+                if (myStack.peek() != '[')
+                    return false;
+                else
+                    myStack.pop();
+            }
+
+            if (c == '}'){
+                if (myStack.peek() != '{')
+                    return false;
+                else
+                    myStack.pop();
+            }
+        }
+
+
+        return myStack.isEmpty(); //最后不能直接返回true，需要检查栈中是否还存在元素
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("(new Valid_Parentheses_20).isValid(\"()[]{}\") = " + (new Valid_Parentheses_20()).isValid("()[]{}"));
+    }
+
+
 }
