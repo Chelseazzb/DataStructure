eaedea4708997f69803cf56c0e9a8307423835d5
Kevin <1147623296@qq.com>
2021-08-27 14:28:23 +0800

    完成了集合的文件读取测试

--- Set/src/per/zzb/set/Main.java
+++ Set/src/per/zzb/set/Main.java
@@ -1,5 +1,7 @@
 package per.zzb.set;
 
+import java.util.ArrayList;
+
 /**
  * @ClassName : Main
  * @Author : Zhai Zhibin
@@ -9,6 +11,28 @@ package per.zzb.set;
  * @Version : 1.0
  **/
 public class Main {
+
+    public void getCount(Set<String> set, String filename){
+
+        ArrayList<String> list = new ArrayList<>();
+
+        if (FileOperation.readFile(filename,list)){
+
+            System.out.println("TotalSize of "+ filename + "is "+list.size());
+
+            Long startTime = System.nanoTime();
+            for (String str : list){
+                set.add(str);
+            }
+
+            System.out.println("Non-repeated word size is " + set.getSize());
+
+            Long endTime = System.nanoTime();
+
+            System.out.println("Time："+ (double)(endTime - startTime)/1000000000 + "s");
+        }
+
+    }
 
     public static void main(String[] args) {
         //BSTSet的测试用例
@@ -24,15 +48,21 @@ public class Main {
 //        System.out.println(bstSet);
 
         //LinkedListSet的测试用例
-        LinkedListSet<Integer> linkedListSet = new LinkedListSet<>();
-        System.out.println("BST树是否为空：" + linkedListSet.isEmpty());
+//        LinkedListSet<Integer> linkedListSet = new LinkedListSet<>();
+//        System.out.println("BST树是否为空：" + linkedListSet.isEmpty());
+//
+//        for (int i = 0; i < 10; i++) {
+//            linkedListSet.add(i);
+//        }
+//
+//        linkedListSet.delete(1);
+//        System.out.println("BST树节点数量：" + linkedListSet.getSize());
+//        System.out.println(linkedListSet);
 
-        for (int i = 0; i < 10; i++) {
-            linkedListSet.add(i);
-        }
+        LinkedListSet<String> set1 = new LinkedListSet<>();
+        BSTSet<String> set2 = new BSTSet<>();
 
-        linkedListSet.delete(1);
-        System.out.println("BST树节点数量：" + linkedListSet.getSize());
-        System.out.println(linkedListSet);
+        new Main().getCount(set1,"pride-and-prejudice.txt");
+        new Main().getCount(set2,"pride-and-prejudice.txt");
     }
 }
