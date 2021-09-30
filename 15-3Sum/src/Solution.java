5607da144ba367ffb35bb900a93645f9f27ef0bb
Kevin <1147623296@qq.com>
2021-09-30 14:08:05 +0800

    增加了main方法

--- 15-3Sum/src/Solution.java
+++ 15-3Sum/src/Solution.java
@@ -1,5 +1,3 @@
-import javax.print.attribute.standard.PresentationDirection;
-import java.beans.PropertyEditorSupport;
 import java.util.*;
 //给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有和为 0 且不重复的三元组。
 //注意：答案中不可以包含重复的三元组。
@@ -33,5 +31,9 @@ class Solution {
             }
         }
         return ans;
+    }
+
+    public static void main(String[] args) {
+
     }
 }
