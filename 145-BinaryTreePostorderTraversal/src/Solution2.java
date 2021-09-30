f621ccd8a16187b12f77ea09f874bff44bf941e2
Kevin <1147623296@qq.com>
2021-09-30 13:58:40 +0800

    使用标记法完成后序遍历

---
+++ 145-BinaryTreePostorderTraversal/src/Solution2.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * @ClassName : Solution2
+ * @Author : Zhai Zhibin
+ * @Date : 2021/9/26 21:22
+ * @Description : 使用标记法实现后序遍历，统一前中后序的方式
+ * @Modified_by :
+ * @Version : 1.0
+ **/
+public class Solution2 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+
+        if (root != null)
+            stack.push(root);
+
+        while (!stack.isEmpty()){
+
+            TreeNode node = stack.peek();
+            if (node != null){
+
+            } else {
+                stack.pop(); //弹出空节点
+                list.add(stack.pop().val);
+            }
+
+        }
+        return list;
+
+    }
+}
