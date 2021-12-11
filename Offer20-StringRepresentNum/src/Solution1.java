5574401ef17d34463c3d99331959a77335c0a74e
Kevin <1147623296@qq.com>
2021-12-11 19:42:17 +0800

    完成剑指Offer20题-字符串表示数字

--- Offer20-StringRepresentNum/src/Solution1.java
+++ Offer20-StringRepresentNum/src/Solution1.java
@@ -11,13 +11,13 @@ public class Solution1 {
         }
         int cur = index;
         index = scanUnsignedInteger(arr, index);
-        boolean numeric = (index > cur);
-        if (index < arr.length && arr[index] == '.') {
+        boolean numeric = (index > cur); //判断是否有数字
+        if (index < arr.length && arr[index] == '.') { //会先出现小数点之后再出现e/E
             //略过小数点
             index++;
             cur = index;
             index = scanUnsignedInteger(arr, index);
-            numeric = (index > cur) || numeric;
+            numeric = (index > cur) || numeric; //可以是三种情况，前面有，后面有或者前后都有
         }
         if (index < arr.length && (arr[index] == 'e' || arr[index] == 'E')) {
             //略过e/E
@@ -28,11 +28,12 @@ public class Solution1 {
             }
             cur = index;
             index = scanUnsignedInteger(arr, index);
-            numeric = (index > cur) && numeric;
+            numeric = (index > cur) && numeric; //前面是数字，而且后面也有数字
         }
-        return numeric && index == arr.length;
+        return numeric && index == arr.length; //是数字，且全部遍历完成
     }
 
+    //跳过数字，并返回非字符的第一个位置
     public int scanUnsignedInteger(char[] arr, int index) {
         while (index < arr.length && arr[index] >= '0' && arr[index] <= '9') {
             index++;
