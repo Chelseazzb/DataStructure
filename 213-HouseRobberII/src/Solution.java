f3ce252155bc3f9a5feaaeb960fbc8f55769ba25
Kevin <1147623296@qq.com>
2021-12-15 22:45:21 +0800

    完成力扣213题-打家劫舍II

---
+++ 213-HouseRobberII/src/Solution.java
@@ -0,0 +1,49 @@
+//你是一个专业的小偷，计划偷窃沿街的房屋，每间房内都藏有一定的现金。
+// 这个地方所有的房屋都围成一圈 ，这意味着第一个房屋和最后一个房屋是紧挨着的。
+// 同时，相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
+
+//给定一个代表每个房屋存放金额的非负整数数组，计算你 在不触动警报装置的情况下 ，今晚能够偷窃到的最高金额。
+
+public class Solution {
+
+    //1 <= nums.length <= 100
+    //0 <= nums[i] <= 1000
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return 0;
+        int len = nums.length;
+        if (len == 1)
+            return nums[0];
+        return Math.max(robAction(nums, 0, len - 1), robAction(nums, 1, len));
+    }
+
+    int robAction(int[] nums, int start, int end) {
+        int prevPrev = 0, prev = 0, cur = 0;
+        for (int i = start; i < end; i++) {
+            prev = cur;
+            cur = Math.max(prev, prevPrev + nums[i]);
+            prevPrev = prev;
+        }
+        return cur;
+    }
+
+    //不进行空间优化的打家劫舍
+    public int robRange(int[] nums, int start, int end) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        if (start == end) {
+            return nums[start];
+        }
+        int[] dp = new int[nums.length];
+        dp[start] = nums[start];
+        dp[start + 1] = Math.max(nums[start], nums[start + 1]);
+        for (int i = start + 2; i <= end; i++) {
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+        return dp[end];
+    }
+}
